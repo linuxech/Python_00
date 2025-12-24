ac572ec80a0fa1e825ec0aa97833946916ae9937
Ashish Prasad <42958334+linuxech@users.noreply.github.com>
2025-12-24 18:49:37 +0000

    String and how to modifying string

---
+++ learn-python/String.py
@@ -0,0 +1,11 @@
+#Ask user for their name
+name =  input("What's is your name? ").strip().title()
+
+#Remove whitespace from str and capitalize the user's name
+#name = name.strip().title() #Merge the strip and title
+
+#Capitalize user's name
+#name = name.title()  -- 1st approach
+
+#Say hello to user
+print (f"hello, {name}")        #Use data type to clean the data 
--- learn-python/hello4.py
+++ learn-python/hello4.py
@@ -2,5 +2,4 @@
 name = input("What's your name? ")
 
 #say hello to user
-print (f"hello, {name}")    #the f in front will allow the python to consider
-                            #this is a special function 'name'
+print (f"hello, {name}")    #the f in front will allow the python to consider this is a special function 'name'.
